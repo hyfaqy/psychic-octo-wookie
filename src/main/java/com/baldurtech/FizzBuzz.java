f555467a80082aa2747b71501443b73ca211d8db
hyfaqy <hyfaqy@gmail.com>
2014-08-05 11:07:42 +0800

    新建一个简单的FizzBuzz.java文件

---
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -0,0 +1,12 @@
+package com.baldurtech;
+
+public class FizzBuzz {
+
+    public String say(Integer num) {
+        return null;
+    }
+    public static void main(String args[]) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.say(1));
+    }
+}
