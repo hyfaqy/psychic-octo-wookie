d1e8aa2264825114315b0f09d5f093145af55b8c
hyfaqy <hyfaqy@gmail.com>
2014-08-05 15:40:01 +0800

    通过修改条件可以让整出3的数都可以输出Fizz

--- src/main/java/com/baldurtech/FizzBuzz.java
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -3,7 +3,7 @@ package com.baldurtech;
 public class FizzBuzz {
 
     public String say(Integer num) {
-        if(num == 3) {
+        if(num % 3 == 0 ) {
             return "Fizz";
         }
         return String.valueOf(num);
