67d477583959739a1bb6dc556ba98d40acf8dfae
hyfaqy <hyfaqy@gmail.com>
2014-08-06 21:09:56 +0800

    进一步对代码进行调整，删除多余部分。

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -5,20 +5,22 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class FizzBuzzTest {
- 
+
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    
     static Boolean testResult = true;
     
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) {
         runAllTests(FizzBuzzTest.class);
         testReport();
     }
     
-    private static void runAllTests(Class clazz) throws Exception {
+    private static void runAllTests(Class clazz) {
         for(Method method: getAllTestMethods(clazz)) {
             System.out.println("testing: " + method.getName());
             try {
-            Object obj = clazz.newInstance();
-            method.invoke(obj, new Object[]{});  
+                Object obj = clazz.newInstance();
+                method.invoke(obj, new Object[]{});  
             } catch(Exception e) {
                 testResult = false;
                 System.out.println("Unexpected exception!");
@@ -27,11 +29,15 @@ public class FizzBuzzTest {
         }
     }
     
-    public static void assertEquals(String expectedResult, String actualResult) {
-            if(expectedResult.equals(actualResult) == false) {
-                testResult = false;
-                System.out.println("Expeccted Result : " + expectedResult + ", but Actual Result:" + actualResult);
+    private static List<Method> getAllTestMethods(Class clazz) {
+        List<Method> testMethods = new ArrayList<Method>();
+        Method[] methods = clazz.getDeclaredMethods();
+        for(Method method: methods) {
+            if(method.getName().startsWith("test")) {
+                testMethods.add(method);
             }
+        }
+       return testMethods;
     }
     
     public static void testReport() {
@@ -42,7 +48,13 @@ public class FizzBuzzTest {
         }
     }
     
-    FizzBuzz fizzBuzz = new FizzBuzz();
+    public static void assertEquals(String expectedResult, String actualResult) {
+            if(expectedResult.equals(actualResult) == false) {
+                testResult = false;
+                System.out.println("Expected: " + expectedResult + ", but:" + actualResult);
+            }
+    }
+    
     public void test1_should_be_1() {
         assertEquals("1", fizzBuzz.say(1));
     }
@@ -66,15 +78,6 @@ public class FizzBuzzTest {
         assertEquals("4", fizzBuzz.say(4));
     }
     
-    private static List<Method> getAllTestMethods(Class clazz) {
-        List<Method> testMethods = new ArrayList<Method>();
-        Method[] methods = FizzBuzzTest.class.getDeclaredMethods();
-        for(Method method: methods) {
-            if(method.getName().startsWith("test")) {
-                testMethods.add(method);
-            }
-        }
-       return testMethods;
-    }
+    
         
 }
