411737882902d3326d8e616cc430dfd90d74b5b3
hyfaqy <hyfaqy@gmail.com>
2014-08-08 23:47:18 +0800

    添加了2的成功测试。

--- src/main/java/com/baldurtech/FizzBuzz.java
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -2,15 +2,22 @@ package com.baldurtech;
 
 class FizzBuzz {
     public String say(Integer num) {
-        return "1"; 
+        return String.valueOf(num);
     }
 
     public static void main(String args[]) {
         FizzBuzz fizzBuzz = new FizzBuzz();
+        
         if("1".equals(fizzBuzz.say(1))) {
             System.out.println("Test Success");
         } else {
             System.out.println("Expect 1 but" + fizzBuzz.say(1));
+        }
+        
+        if("2".equals(fizzBuzz.say(2))) {
+            System.out.println("Test Success");
+        } else {
+            System.out.println("Expect 2 but" + fizzBuzz.say(2));
         }
     }
 }
