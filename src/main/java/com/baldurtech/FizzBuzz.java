71f72e473795f14f8dc9c68079a8235261fa9b95
hyfaqy <hyfaqy@gmail.com>
2014-08-09 00:23:08 +0800

    把判断是否相等的部分抽成方法。

--- src/main/java/com/baldurtech/FizzBuzz.java
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -1,25 +1,16 @@
 package com.baldurtech;
 
 class FizzBuzz {
+    static boolean testResult = false;
     public String say(Integer num) {
         return String.valueOf(num);
     }
 
     public static void main(String args[]) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        boolean testResult = false;
         
-        if("1".equals(fizzBuzz.say(1)) != false) {
-            testResult = true;
-        } else {
-            System.out.println("Expected 1 but" + fizzBuzz.say(1));
-        }
-        
-        if("2".equals(fizzBuzz.say(2)) != false) {
-            testResult = true;
-        } else {
-            System.out.println("Expected 2 but" + fizzBuzz.say(2));
-        }
+        testResult = assertEquals("1", fizzBuzz.say(1));
+        testResult = assertEquals("2", fizzBuzz.say(2));
         
         if(testResult) {
             System.out.println("Test Success");
@@ -27,4 +18,13 @@ class FizzBuzz {
             System.out.println("Test False");
         }
     }
+    
+    public static boolean assertEquals(String expectedResult, String actualResult) {
+        if(expectedResult.equals(actualResult)) {
+            testResult = true;
+        } else {
+            System.out.println("Expected" + expectedResult + "but" + actualResult);
+        }
+        return testResult;
+    }  
 }
