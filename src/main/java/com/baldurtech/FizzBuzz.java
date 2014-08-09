cf30a35a14ae85822f81a92173f07c1d98d38fcb
hyfaqy <hyfaqy@gmail.com>
2014-08-09 09:33:08 +0800

    增加了一个Bug，本来想让它影响以后的其他测试，但是没有起到作用。

--- src/main/java/com/baldurtech/FizzBuzz.java
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -2,7 +2,12 @@ package com.baldurtech;
 
 class FizzBuzz {
     public String say(Integer num) {
+        boolean thisIsABug = false;
+        if(num == 2) thisIsABug = true;
         if(num%3 == 0) {
+            if(thisIsABug) {
+                return "42";
+            }
             return "Fizz";
         }
         return String.valueOf(num);
